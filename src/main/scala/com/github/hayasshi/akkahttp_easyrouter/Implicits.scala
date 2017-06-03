3b243bb6051dbf584d956fac0eebb291e28c7b83
hayasshi <daisuke0884@gmail.com>
2017-06-03 16:42:41 +0900

    Rename implicit converters

--- src/main/scala/com/github/hayasshi/akkahttp_easyrouter/Implicits.scala
+++ src/main/scala/com/github/hayasshi/akkahttp_easyrouter/Implicits.scala
@@ -4,8 +4,8 @@ import akka.http.scaladsl.server.Route
 
 trait Implicits {
 
-  implicit def argToUnit(r: Route): Unit => Route = _ => r
+  implicit def toUnitArgFunction1(r: Route): Unit => Route = _ => r
 
-  implicit def argToTuple1[A](ar: A => Route): Tuple1[A] => Route = ar.compose(_._1)
+  implicit def toTuple1ArgFunction1[A](ar: A => Route): Tuple1[A] => Route = ar.compose(_._1)
 
 }
