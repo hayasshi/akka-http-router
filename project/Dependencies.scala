0371038df7568589a0fd7024904c7ee6d0094aca
Daisuke Hayashi <daisuke0884@gmail.com>
2019-11-16 01:13:56 +0900

    Update akka-http, akka-http-testkit to 10.1.10 (#23)
    * Update akka-http, akka-http-testkit to 10.1.10
    
    * Fix dependencies for akka-http 10.1.10
    
    * Fix nits issues

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,16 +3,16 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    // same version as akka-http
     val crossScalaVersions = Seq("2.12.10", "2.11.12")
     val scalaVersion       = crossScalaVersions.head
     val akkaVersion        = "2.5.26"
-    val akkaHttpVersion    = "10.1.4"
+    val akkaHttpVersion    = "10.1.10"
   }
 
-  lazy val akkaStream      = "com.typesafe.akka" %% "akka-stream" % Versions.akkaVersion
-  lazy val akkaHttp        = "com.typesafe.akka" %% "akka-http" % Versions.akkaHttpVersion
-  lazy val akkaHttpTestKit = "com.typesafe.akka" %% "akka-http-testkit" % Versions.akkaHttpVersion
+  lazy val akkaStream        = "com.typesafe.akka" %% "akka-stream" % Versions.akkaVersion
+  lazy val akkaStreamTestKit = "com.typesafe.akka" %% "akka-stream-testkit" % Versions.akkaVersion
+  lazy val akkaHttp          = "com.typesafe.akka" %% "akka-http" % Versions.akkaHttpVersion
+  lazy val akkaHttpTestKit   = "com.typesafe.akka" %% "akka-http-testkit" % Versions.akkaHttpVersion
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.8"
 
