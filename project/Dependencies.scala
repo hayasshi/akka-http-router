f77366c9755f16f42a22097d405cb41559fcc59f
Scala Steward <me@scala-steward.org>
2021-11-22 17:26:19 +0100

    Update akka-http, akka-http-testkit to 10.1.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,15 +4,15 @@ object Dependencies {
 
   object Versions {
     val crossScalaVersions = Seq("2.13.1", "2.12.10", "2.11.12")
-    val scalaVersion       = crossScalaVersions.head
-    val akkaVersion        = "2.5.27"
-    val akkaHttpVersion    = "10.1.11"
+    val scalaVersion = crossScalaVersions.head
+    val akkaVersion = "2.5.27"
+    val akkaHttpVersion = "10.1.15"
   }
 
-  lazy val akkaStream        = "com.typesafe.akka" %% "akka-stream" % Versions.akkaVersion
+  lazy val akkaStream = "com.typesafe.akka" %% "akka-stream" % Versions.akkaVersion
   lazy val akkaStreamTestKit = "com.typesafe.akka" %% "akka-stream-testkit" % Versions.akkaVersion
-  lazy val akkaHttp          = "com.typesafe.akka" %% "akka-http" % Versions.akkaHttpVersion
-  lazy val akkaHttpTestKit   = "com.typesafe.akka" %% "akka-http-testkit" % Versions.akkaHttpVersion
+  lazy val akkaHttp = "com.typesafe.akka" %% "akka-http" % Versions.akkaHttpVersion
+  lazy val akkaHttpTestKit = "com.typesafe.akka" %% "akka-http-testkit" % Versions.akkaHttpVersion
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.0"
 
